457dd44aee07ea4483c61c2011b8157f71f11c93
Nikhil Marathe <nsm.nikhil@gmail.com>
2016-02-19 16:15:28 -0800

    Add support to access context.functionName and context.functionVersion.
    swapi does not allow $ in variables, so cheating on $LATEST right now.

--- test-suite/tests/node/test-context/test.js
+++ test-suite/tests/node/test-context/test.js
@@ -1,23 +1,31 @@
+var assert = require('assert');
 exports.run = function(event, context) {
   // context.succeed.
-  console.log("context.succeed is a function",
-              typeof context.succeed == 'function');
-  console.log("context.succeed takes one argument", context.succeed.length === 1);
+  assert.ok(typeof context.succeed == 'function',
+              "context.succeed is a function");
+  assert.ok(context.succeed.length === 1, "context.succeed takes one argument");
 
   // context.fail.
-  console.log("context.fail is a function", typeof context.fail == 'function');
-  console.log("context.fail takes one argument", context.fail.length === 1);
+  assert.ok(typeof context.fail == 'function', "context.fail is a function");
+  assert.ok(context.fail.length === 1, "context.fail takes one argument");
 
   // context.done.
   // Although done accepts 2 arguments (both optional), the length value is
   // 0 on Lambda.
-  console.log("context.done is a function", typeof context.done == 'function');
-  console.log("context.done takes zero arguments", context.done.length === 0);
+  assert.ok(typeof context.done == 'function', "context.done is a function");
+  assert.ok(context.done.length === 0, "context.done takes zero arguments");
 
   // context.getRemainingTimeInMillis.
-  console.log("context.getRemainingTimeInMillis is a function",
-              typeof context.getRemainingTimeInMillis == 'function')
-  console.log("context.getRemainingTimeInMillis takes zero arguments",
-              context.getRemainingTimeInMillis.length === 0)
+  assert.ok(typeof context.getRemainingTimeInMillis == 'function',
+            "context.getRemainingTimeInMillis is a function");
+  assert.ok(context.getRemainingTimeInMillis.length === 0,
+            "context.getRemainingTimeInMillis takes zero arguments");
 
+  // We can't check for equality since the AWS and Iron naming convention is different.
+  assert.ok(typeof context.functionName == 'string' && context.functionName.match('context'), "context.functionName contains 'context'.");
+
+  // We only support latest for now.
+  assert.ok(typeof context.functionVersion == 'string' && context.functionVersion == "$LATEST", "context.functionVersion is $LATEST");
+
+  assert.ok(typeof context.awsRequestId == 'string' && context.awsRequestId.length > 0, "context.awsRequestId is defined.");
 }
