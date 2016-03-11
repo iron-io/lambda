366d31271a49fb2bbf71dd5f2e04842f34626f8b
Vitaly Lopatkin <vitaly.lopatkin@gmail.com>
2016-03-11 15:35:07 +0600

    installing bsddb3 & urlgrabber and test that

--- test-suite/tests/python/test-versions/test.py
+++ test-suite/tests/python/test-versions/test.py
@@ -1,10 +1,39 @@
 from __future__ import print_function
 import sys
-import boto3
 
 
 def run(event, context):
     print ("python version {major}.{minor}"
         .format(major=sys.version_info[0], minor=sys.version_info[1]))
-    print ("boto3 version {version}"
-        .format(version=boto3.__version__))
+
+    modules = [
+        'boto3',
+        'botocore',
+        {'key':'bsddb', 'packages': ['bsddb', 'bsddb3' ]},
+        'urlgrabber'
+    ]
+    for entry in modules:
+        moduleName = getModuleName(entry)
+        module = getModule(entry)
+        print ("{moduleName} version {version}"
+            .format(moduleName=moduleName, version=module.__version__))
+
+
+def getModuleName(entry):
+    return (isinstance(entry, dict) and entry['key']) or entry
+
+
+def getModule(entry):
+    if isinstance(entry, dict):
+        for name in entry['packages']:
+            try:
+                return getModuleByName(name)
+            except:
+                pass
+        raise Exception('no module %s found' % entry['key'])
+    else:
+        return getModuleByName(entry)
+
+
+def getModuleByName(name):
+    return sys.modules.get(name, None) or __import__(name)
